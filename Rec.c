f59a0a84aaa1594d0c17b5a671b03e1585e5d9ed
Manthan Kallure <71187023+Manthan-19@users.noreply.github.com>
2022-08-01 23:04:51 +0530

    Add files via upload

---
+++ Rec.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void DisplayI(int no)
+{
+	int i = 1;
+	while(i <= no)
+	{
+		printf("*\t");
+		i++;
+	}
+}
+
+void DisplayR(int no)
+{
+	static int i = 1;
+	if(i <= no)
+	{
+		printf("*\t");
+		i++;
+		DisplayR(no);	//Recursive call
+	}
+}
+
+int main()
+{
+	int value = 3;
+	DisplayR(value); 
+
+	return 0;
+}
